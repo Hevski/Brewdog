abb27958f67fdac9c5ea6e8d00e3f0a73edca1f1
Heather <heathercdavies@gmail.com>
2019-02-19 16:41:09 +0000

    initial set up and can display chosen beer

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: "#app",
+    data: {
+      beers: [],
+      selectedBeerIndex: '',
+      selectedBeer: null,
+      favoriteBeers: []
+    },
+    mounted: function(){
+      this.getBeers();
+    },
+    methods: {
+      getBeers: function(){
+        fetch('https://api.punkapi.com/v2/beers')
+        .then(response => response.json())
+        .then(data => this.beers = data)
+      },
+      beerSelect: function(){
+        this.selectedBeer = this.beers[this.selectedBeerIndex]
+      },
+      saveFavoriteBeer: function(){
+        this.favoriteBeers.push(this.selectedBeer)
+      }
+    }
+  })
+});
